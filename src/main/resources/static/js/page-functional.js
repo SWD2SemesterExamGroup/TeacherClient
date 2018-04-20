b8b209acbab855aab9d4c0fe4dc9eb6bafacf776
Troels H. Jensen <lord_ni@hotmail.com>
2018-04-20 23:12:33 +0200

    change settings and port to 801

--- src/main/resources/static/js/page-functional.js
+++
@@ -1,8 +0,0 @@
-/* All overall page creations */
-
-function includeHTML() {
-    var elmnt = document.getElementById("menu-bar");
-    var attribute = elmnt.getAttribute("include-html");
-    console.log(attribute);
-    return;
-}
